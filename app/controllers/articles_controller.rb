3b234f087de77e9b3c88661fb60400f070482c8d
Yamamori <moriyama14t@gmail.com>
2023-01-08 13:51:58 +0900

    記事一覧所得機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
+        format.html { redirect_to articles_url, notice: "Article was successfully created." }
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new, status: :unprocessable_entity }
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ApplicationRecord
   # belongs_to :user
+
+  require 'open-uri'
+  require 'nokogiri'
+  
+  def loadInfo
+    html = URI.open(url).read
+    doc = Nokogiri::HTML.parse(html)
+  end
 end
