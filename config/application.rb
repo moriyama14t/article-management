e8f5a0d73cd08d1d88ca84de7447968b058d6895
Yamamori <moriyama14t@gmail.com>
2023-01-09 11:16:04 +0900

    render へのデプロイ

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module ArticleManagement
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # デプロイするサービスのホストを追加する
+    config.hosts << 'article-management.onrender.com'
   end
 end
